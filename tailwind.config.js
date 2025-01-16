654e6ada49610e3fa608616d0057cd3097655f84
Vikasgamersback <vikas@gamersback.com>
2025-01-16 21:52:38 +0530

    public uploaded and rest file uploaded

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "header-light": "#ffffff",
+        "header-dark": "#393939",
+        "section-light": "#F1F1F9",
+        "section-dark": "#242424",
+      },
+    },
+  },
+  plugins: [],
+};
